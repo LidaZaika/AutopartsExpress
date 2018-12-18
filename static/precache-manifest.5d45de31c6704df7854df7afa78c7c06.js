ded9e03ecbf7883f8eb309f4b4a9272a35337b06
LidaZaika <2501987@mail.ru>
2018-12-18 18:01:10 +0300

    fetch-BD

---
+++ static/precache-manifest.5d45de31c6704df7854df7afa78c7c06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db35e141a14447e3d94e",
+    "url": "/static/js/main.db35e141.chunk.js"
+  },
+  {
+    "revision": "2fb2213ea2dc6529b486",
+    "url": "/static/js/1.2fb2213e.chunk.js"
+  },
+  {
+    "revision": "db35e141a14447e3d94e",
+    "url": "/static/css/main.bff4bc7f.chunk.css"
+  },
+  {
+    "revision": "b44ab023a29d747fe719259d5284783f",
+    "url": "/index.html"
+  }
+];
